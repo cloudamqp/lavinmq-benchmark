85393f284893ec3c88d7dbb309e53e6059bd228a
Tobias Brodén <tobias@84codes.com>
2025-02-21 15:43:16 +0100

    Enable multiple version runs

--- configuration/locals.tf
+++ configuration/locals.tf
@@ -1,13 +1,13 @@
 # Local variavles to configure instances used to be benchmarked and running perftest
 locals {
-  benchmark_instance_plan    = "penguin-1"
-  benchmark_instance_version = null
-  perftest_instance_plan     = "lynx-1"
-  region                     = "amazon-web-services::eu-central-1"
-  tags                       = ["lavinmq", "benchmark"]
+  benchmark_plan     = "penguin-1"
+  benchmark_versions = tolist([null])
+  perftest_plan      = "lynx-1"
+  region             = "azure-arm::northeurope"
+  tags               = ["lavinmq", "benchmark"]
 }
 
 # LavinMQ Perf test command. --uri= will be poplated in terraform_data.perftest_command
 locals {
-  perftest = "lavinmqperf throughput -z 5 -x 1 -y 1 -s 16"
+  perftest = "lavinmqperf throughput -z 120 -x 1 -y 1 -s 16"
 }
--- configuration/main.tf
+++ configuration/main.tf
@@ -8,33 +8,40 @@ terraform {
 
 # Standalone VPC shared between the instances
 resource "cloudamqp_vpc" "vpc" {
-  name    = "lavinmq-benchmark"
-  subnet  = "10.56.72.0/24"
-  region  = local.region
-  tags    = local.tags
+  name   = "lavinmq-benchmark"
+  subnet = "10.56.72.0/24"
+  region = local.region
+  tags   = local.tags
 }
 
 # Instance being benchmarked
 resource "cloudamqp_instance" "instance" {
-  name                = "lavinmq-benchmark-${random_string.this.result}"
-  plan                = local.benchmark_instance_plan
+  count = length(local.benchmark_versions)
+
+  name                = "lavinmq-benchmark-${count.index}"
+  plan                = local.benchmark_plan
   region              = local.region
   tags                = local.tags
-  rmq_version         = local.benchmark_instance_version
-  vpc_id	            = cloudamqp_vpc.vpc.id
+  rmq_version         = local.benchmark_versions[count.index]
+  vpc_id              = cloudamqp_vpc.vpc.id
   keep_associated_vpc = true
-}
-
-data "cloudamqp_nodes" "perftest-nodes" {
-  instance_id = cloudamqp_instance.instance.id
 }
 
 # Instance running perftest
 resource "cloudamqp_instance" "perftest" {
+  count = length(local.benchmark_versions)
+
   name                = "lavinmqperftest"
-  plan                = local.perftest_instance_plan
+  plan                = local.perftest_plan
   region              = local.region
   tags                = local.tags
-  vpc_id	            = cloudamqp_vpc.vpc.id
+  vpc_id              = cloudamqp_vpc.vpc.id
   keep_associated_vpc = true
+}
+
+# Perftest nodes
+data "cloudamqp_nodes" "perftest-nodes" {
+  count = length(cloudamqp_instance.perftest)
+
+  instance_id = cloudamqp_instance.perftest[count.index].id
 }
