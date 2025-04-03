91e661bdb6e86b8d2aea5019329fbcca2b2773f9
Tobias Brodén <47317658+tbroden84@users.noreply.github.com>
2025-04-03 10:44:31 +0200

    Specify LavinMQ version to bootstrap (#6)
    ### WHY are these changes introduced?
    
    Benchmark servers will be created with latest LavinMQ version.
    
    Close: https://github.com/cloudamqp/lavinmq-benchmark/issues/5
    
    ### WHAT is this pull request doing?
    
    Introduces a new variable to specify a specific LavinMQ version.
    Default uses the latest version.

--- AWS/main.tf
+++ AWS/main.tf
@@ -9,6 +9,7 @@ module "benchmark" {
   instance_type     = var.benchmark_instance_type
   instance_name     = var.benchmark_name
   ami_arch          = var.ami_arch
+  lavinmq_version   = var.lavinmq_version
   tag_created_by    = var.tag_created_by
   subnet_id         = aws_subnet.subnet.id
   ssh_key_pair_name = aws_key_pair.ssh_key.key_name
@@ -20,6 +21,7 @@ module "perftest" {
   instance_type     = var.perftest_instance_type
   instance_name     = var.perftest_name
   ami_arch          = var.ami_arch
+  lavinmq_version   = var.lavinmq_version
   tag_created_by    = var.tag_created_by
   subnet_id         = aws_subnet.subnet.id
   ssh_key_pair_name = aws_key_pair.ssh_key.key_name
