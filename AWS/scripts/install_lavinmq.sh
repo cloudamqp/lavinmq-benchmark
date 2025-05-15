788c83e9d18fb5cf6a52262ddad91ddfa88a00fe
Tobias Brodén <tobias@84codes.com>
2025-05-15 13:48:43 +0200

    Don't silence StdErr during commands

--- AWS/scripts/install_lavinmq.sh
+++ AWS/scripts/install_lavinmq.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -xe
 
-apt-get update -qq > /dev/null 2>&1
+apt-get update -qq > /dev/null
 curl -fsSL https://packagecloud.io/cloudamqp/lavinmq/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/lavinmq.gpg > /dev/null
 . /etc/os-release
 echo "deb [signed-by=/usr/share/keyrings/lavinmq.gpg] https://packagecloud.io/cloudamqp/lavinmq/$ID $VERSION_CODENAME main" | sudo tee /etc/apt/sources.list.d/lavinmq.list > /dev/null
-apt-get update -qq > /dev/null 2>&1
+apt-get update -qq > /dev/null
 
 if [ -n "${LAVINMQ_VERSION}" ]
 then apt-get install "lavinmq=${LAVINMQ_VERSION}-1" -y --allow-change-held-packages -qq > /dev/null
 else apt-get install lavinmq -y --allow-change-held-packages -qq > /dev/null
 fi
 
-apt-get autoremove -y -qq > /dev/null 2>&1
+apt-get autoremove -y -qq > /dev/null
